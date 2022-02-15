212fd7c6ce1aa1f6a87ebb62027137e4ac873788
Zackary Hopkins <zhopkins@mail.bradley.edu>
2022-02-15 15:49:45 -0600

    Finished Prototype3

--- Prototype3/Assets/Scripts/PlayerController.cs
+++ Prototype3/Assets/Scripts/PlayerController.cs
@@ -8,14 +8,26 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
     public ForceMode forceMode;
     public float gravityModifier;
-
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
     public bool gameOver = false;
     public bool isOnGround = true;
+
+    public Animator playerAnimator;
+
+    private AudioSource playerAudio;
     // Start is called before the first frame update
     void Start()
     {
         //set a ref to the rigidbody comp
         rb = GetComponent<Rigidbody>();
+        playerAnimator = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+        //start animation
+        playerAnimator.SetFloat("Speed_f", 1.0f);
+
         forceMode = ForceMode.Impulse;
 
         if (Physics.gravity.y > -10)
@@ -29,22 +41,29 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
             rb.AddForce(Vector3.up * jumpForce, forceMode);
+            playerAnimator.SetTrigger("Jump_trig");
             isOnGround = false;
         }
-
-
     }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
+            dirtParticle.Play();
             isOnGround = true;
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
             Debug.Log("gameover");
             gameOver = true;
+            //death animation
+            playerAudio.PlayOneShot(crashSound, 1.0f);
+            playerAnimator.SetBool("Death_b", true);
+            playerAnimator.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
         }
     }
 }
